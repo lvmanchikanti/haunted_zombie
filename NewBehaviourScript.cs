ba0e92ba303d74ebc0ed292f1c819df9f28d4228
Lahari Manchikanti <lahumanchik@gmail.com>
2017-08-11 10:52:16 -0500

    initial commit C# script

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+	[SerializeField] float objectSpeed = 1;
+	private float resetPosition = -21.65f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime));
+
+		if (transform.localPosition.x <= resetPosition) {
+			Vector3 newPos = new Vector3 (79.67f, transform.position.y, transform.position.z);
+			transform.position = newPos;
+		}
+	}
+}
